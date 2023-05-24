3090ccea4490af90d4478b7046979eda15ef6d4f
PC AS <aldazpaulo@gmail.com>
2023-05-23 19:43:59 -0500

    bindings

---
+++ app/src/main/java/com/exam/dispositivosmoviles/ui/activities/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.exam.dispositivosmoviles
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.SearchView
+import android.widget.TextView
+import com.exam.dispositivosmoviles.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    var a = 2
+
+    @SuppressLint("MissingInflateId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        initClass()
+
+    }
+
+    private fun initClass() {
+        var boton1 = binding.button
+        var txtBuscar = binding.textView3
+        boton1.elevation
+
+        binding.button.setOnClickListener{
+            boton1.text="Hola"
+            txtBuscar.text="Buscando..."
+            binding.textView3.text = "El Codigo ejecutado"
+            var f=Snackbar.make(
+                binding.button, "Es mensaje", Snackbar.LENGTH_LONG
+            )
+            f.show()
+        }
+
+
+
+
+
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+
+}
--- app/src/main/java/com/exam/dispositivosmoviles/ui/activities/MainActivity2.kt
+++ app/src/main/java/com/exam/dispositivosmoviles/ui/activities/MainActivity2.kt
@@ -1,11 +1,12 @@
-package com.exam.dispositivosmoviles
+package com.exam.dispositivosmoviles.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.exam.dispositivosmoviles.R
 
-class MainActivity : AppCompatActivity() {
+class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main2)
     }
 }
