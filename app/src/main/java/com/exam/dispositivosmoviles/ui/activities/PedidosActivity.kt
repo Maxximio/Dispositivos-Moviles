9b33517d3e947a708864bb891c7e385abd2433da
driven <aldazpaulo@gmail.com>
2023-06-04 23:36:59 -0500

    probando git

--- app/src/main/java/com/exam/dispositivosmoviles/ui/activities/PedidosActivity.kt
+++ app/src/main/java/com/exam/dispositivosmoviles/ui/activities/PedidosActivity.kt
@@ -1,7 +1,8 @@
-package com.exam.dispositivosmoviles
+package com.exam.dispositivosmoviles.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.exam.dispositivosmoviles.R
 
 class PedidosActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
