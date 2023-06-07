869b919278f411dabb8022d1cd0bfb2570f40df6
PC AS <aldazpaulo@gmail.com>
2023-06-06 19:36:32 -0500

    menu navigation

--- app/src/main/java/com/exam/dispositivosmoviles/ui/activities/MainActivity2.kt
+++ app/src/main/java/com/exam/dispositivosmoviles/ui/activities/MainActivity2.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.util.Log
 import com.exam.dispositivosmoviles.R
 import com.exam.dispositivosmoviles.databinding.ActivityMain2Binding
+import com.google.android.material.snackbar.Snackbar
 
 
 class MainActivity2 : AppCompatActivity() {
@@ -21,13 +22,13 @@ class MainActivity2 : AppCompatActivity() {
     override fun onStart() {
         super.onStart()
 
-        intent.extras.let {
+       /* intent.extras.let {
              var name  = it?.getString("var1")
 //            var name = it.getString("var1")
             Log.d("UCE", "${name} - INTRODUZCA NOMBRE")
 //            var number = it?.getInt("var2")
             binding.respuesta.text = "Bienvenido: " + name.toString()
-        }
+        }*/
 
         Log.d("UCE", "Entrando a Start")
         initClass()
@@ -46,8 +47,36 @@ class MainActivity2 : AppCompatActivity() {
             startActivity(intent)
         }
 
+        binding.bottomNavigation.setOnItemSelectedListener {item ->
+            when(item.itemId) {
+                R.id.item_1 -> {
+                    var suma: Int=0
+
+                    for (i in listOf(1,2,3)){
+                        suma+=1
+                    }
+
+                    Snackbar.make(binding.respuesta,"La suma es: ${suma}",Snackbar.LENGTH_LONG).show()
+                    // Respond to navigation item 1 reselection
+                    true
+                }
+                R.id.item_2 -> {
+                    // Respond to navigation item 2 reselection
+                    Snackbar.make(binding.respuesta,"Entramos a ayuda",Snackbar.LENGTH_LONG).show()
+                    true
+
+                }
+                R.id.item_3 -> {
+                    // Respond to navigation item 2 reselection
+                    Snackbar.make(binding.respuesta,"Entramos a mapa",Snackbar.LENGTH_LONG).show()
+                    true
+                }
+                else -> false
+            }
+        }
 
     }
+
 
 
 
