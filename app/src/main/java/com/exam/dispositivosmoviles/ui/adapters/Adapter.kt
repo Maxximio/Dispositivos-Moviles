d5fd80ed1c04feb0fdf5412f18f9111e275bf70a
PC AS <aldazpaulo@gmail.com>
2023-06-17 08:48:23 -0500

    MarvelChars 2.0

--- app/src/main/java/com/exam/dispositivosmoviles/ui/adapters/Adapter.kt
+++ app/src/main/java/com/exam/dispositivosmoviles/ui/adapters/Adapter.kt
@@ -7,16 +7,27 @@ import androidx.recyclerview.widget.RecyclerView
 import com.exam.dispositivosmoviles.R
 import com.exam.dispositivosmoviles.data.entities.MarvelChars
 import com.exam.dispositivosmoviles.databinding.MarvelCharactersBinding
+import com.google.android.material.snackbar.Snackbar
+import com.squareup.picasso.Picasso
 
 class Adapter(private val items:List<MarvelChars>):RecyclerView.Adapter<Adapter.MarvelViewHolder>() {
     class MarvelViewHolder(view:View): RecyclerView.ViewHolder(view) {
 
         private val binding: MarvelCharactersBinding= MarvelCharactersBinding.bind(view)
 
+//        este es la parte que importa
         fun render(item: MarvelChars){
             binding.marvelT.text=item.nombre
             binding.nameT.text=item.comic
+    binding.imageView.setOnClickListener{
+        Snackbar.make(binding.imageView, item.nombre, Snackbar.LENGTH_SHORT).show()
+    }
+
+//            binding.imageView.
+    Picasso.get().load(item.imagen).into(binding.imageView);
         }
+
+
     }
 
     override fun onCreateViewHolder(
--- app/src/main/java/com/exam/dispositivosmoviles/ui/fragments/FirstFragment.kt
+++ app/src/main/java/com/exam/dispositivosmoviles/ui/fragments/FirstFragment.kt
@@ -5,13 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Adapter
+
 import android.widget.ArrayAdapter
 import android.widget.SimpleAdapter
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView.LayoutManager
 import com.exam.dispositivosmoviles.R
-import com.exam.dispositivosmoviles.databinding.ActivityMainBinding
-import com.exam.dispositivosmoviles.databinding.FragmentFirstBinding
 
+import com.exam.dispositivosmoviles.databinding.FragmentFirstBinding
+import com.exam.dispositivosmoviles.logic.lists.ListItems
+import com.exam.dispositivosmoviles.ui.adapters.Adapter
 
 class FirstFragment : Fragment() {
 
@@ -38,8 +41,12 @@ class FirstFragment : Fragment() {
             R.layout.simple_layout, list)
 
         binding.spinner.adapter = adapter
-        binding.listview.adapter= adapter
+//        binding.listview.adapter= adapter
 
+        val rvAdapter = Adapter(ListItems<Any>().returnMarvelChars())
+        val rvMarvel = binding.rvMarvelChars
+        rvMarvel.adapter = rvAdapter
+        rvMarvel.layoutManager = LinearLayoutManager(requireActivity(), LinearLayoutManager.VERTICAL,false)
     }
 
 }
